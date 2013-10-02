4f69c6a8b0f608080b6eee4a7177eaf5563442a3
Emmanuel Bourgerie <emmanuel.bourgerie@teamaol.com>
2013-10-02 02:41:04 +0100

    Select howto

---
+++ public/js/weusepgp.js
@@ -0,0 +1,33 @@
+$(function() {
+
+  var updateHowTo = function() {
+    var platform = $(".select-platform li.selected").attr("data-platform"),
+        usage = $(".select-usage li.selected").attr("data-usage"),
+        howto = $(".howtos [data-platform="+platform+"][data-usage="+usage+"]"),
+        notAvailable = $(".howtos .not-available");
+
+    $(".howtos li").hide();
+
+    if (howto.length > 0) {
+      notAvailable.hide();
+      howto.show();
+    } else {
+      notAvailable.show();
+    }
+  };
+
+  $("#howto .select-usage").on("click", "li", function(evt) {
+    $("#howto .select-usage li").removeClass("selected");
+    $(evt.target).addClass("selected");
+
+    updateHowTo();
+  });
+
+  $("#howto .select-platform").on("click", "li", function(evt) {
+    $("#howto .select-platform li").removeClass("selected");
+    $(evt.target).addClass("selected");
+
+    updateHowTo();
+  });
+
+});
