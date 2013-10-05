776c8f0fef0801381ea7fd78a212670861d8237b
Emmanuel Bourgerie <emmanuel.bourgerie@teamaol.com>
2013-10-05 23:54:39 +0100

    i18n

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 gem 'sinatra'
 
 gem 'haml'
+gem 'i18n'
 gem 'json'
 gem 'sass'
--- weusepgp.rb
+++ weusepgp.rb
@@ -1,13 +1,39 @@
 require 'rubygems'
+
+require 'i18n'
 require 'json'
 require 'sass'
 require 'sinatra'
 
-get '/' do
+I18n.load_path += Dir[File.join(settings.root, 'tr', '*.yml')]
+
+helpers do
+  def t code
+    I18n.t code
+  end
+end
+
+def home
+  I18n.backend.load_translations
   @howtos = JSON.parse(File.read("howtos.json"))
   haml :index
 end
 
+get '/' do
+  I18n.locale = "en"
+  home
+end
+
 get "/style.css" do
   sass :style
+end
+
+get "/:lng" do
+  unless ["en","fr"].index params[:lng]
+    # 404
+    return
+  end
+
+  I18n.locale = params[:lng]
+  home
 end
